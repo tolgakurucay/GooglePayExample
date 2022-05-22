bdc8b5ec39c17dba487ceb42bfdb1f1329b5e44b
Tolga Kuruçay <tolgakurucay1446@gmail.com>
2022-05-23 01:00:17 +0300

    second commit

--- app/src/main/java/com/tolgakurucay/googlepayexample/MainActivity.kt
+++ app/src/main/java/com/tolgakurucay/googlepayexample/MainActivity.kt
@@ -3,6 +3,7 @@ package com.tolgakurucay.googlepayexample
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 
 import com.android.billingclient.api.*
 import com.tolgakurucay.googlepayexample.databinding.ActivityMainBinding
@@ -25,14 +26,21 @@ class MainActivity : AppCompatActivity() {
             .setListener(object:PurchasesUpdatedListener{
                 override fun onPurchasesUpdated(p0: BillingResult, p1: MutableList<Purchase>?) {
                     if(p0.responseCode==BillingClient.BillingResponseCode.OK && p1!=null){
-                       Log.d("deneme","completed")
+                        Toast.makeText(this@MainActivity,"Billing process has completed",Toast.LENGTH_SHORT).show()
+
                     }
-                    else
-                    {
-                        Log.d("deneme","failed")
+                    else if(p0.responseCode==BillingClient.BillingResponseCode.USER_CANCELED){
+
+                        Toast.makeText(this@MainActivity,"Billing process has canceled",Toast.LENGTH_SHORT).show()
+
                     }
 
+
+
+
+
                 }
+
 
             })
             .build()
@@ -50,10 +58,19 @@ class MainActivity : AppCompatActivity() {
                 }
 
                 override fun onBillingSetupFinished(p0: BillingResult) {
-                    if(p0.responseCode==BillingClient.BillingResponseCode.OK){
-                        //
-                        getProductDetails()
+                    try{
+                        if(p0.responseCode==BillingClient.BillingResponseCode.OK){
+                            getProductDetails()
+                        }
+                        else if(p0.responseCode==BillingClient.BillingResponseCode.USER_CANCELED){
+                            Toast.makeText(this@MainActivity,"Exited the connecting",Toast.LENGTH_SHORT).show()
+                        }
                     }
+                    catch (ex:Exception){
+                        Toast.makeText(this@MainActivity,"Error!\n"+ex.localizedMessage,Toast.LENGTH_SHORT).show()
+                    }
+
+
                 }
 
             }
@@ -63,6 +80,9 @@ class MainActivity : AppCompatActivity() {
     private fun getProductDetails(){
         val productIds= arrayListOf<String>()
         productIds.add("300dollars")
+        productIds.add("100dollar")
+
+
 
         val getProductDetailsQuery=SkuDetailsParams
             .newBuilder()
@@ -74,12 +94,25 @@ class MainActivity : AppCompatActivity() {
        , object : SkuDetailsResponseListener {
                override fun onSkuDetailsResponse(p0: BillingResult, p1: MutableList<SkuDetails>?) {
                    if(p0.responseCode==BillingClient.BillingResponseCode.OK && p1!=null){
-                       val itemInfo=p1.get(0)
-                       binding.ItemName.setText(itemInfo.title)
-                       binding.ItemPrice.setText(itemInfo.price)
-                       binding.ItemPrice.setOnClickListener {
-                           billingClient.launchBillingFlow(this@MainActivity,BillingFlowParams.newBuilder().setSkuDetails(itemInfo).build())
+                       //for gold subs(300 dollars)
+                       val goldItemInfo=p1.get(0)
+
+                       binding.goldSubscriptionTextView.setText(goldItemInfo.title)
+                       binding.goldSubscriptionButton.setText(goldItemInfo.price)
+                       binding.goldSubscriptionButton.setOnClickListener {
+                           billingClient.launchBillingFlow(this@MainActivity,BillingFlowParams.newBuilder().setSkuDetails(goldItemInfo).build())
                        }
+
+                       //for premium subs(100 dollars)
+                       val premiumItemInfo=p1.get(1)
+
+                       binding.premiumSubscriptionTextView.setText(premiumItemInfo.title)
+                       binding.premiumSubscriptionButton.setText(premiumItemInfo.price)
+                       binding.premiumSubscriptionButton.setOnClickListener {
+                           billingClient.launchBillingFlow(this@MainActivity,BillingFlowParams.newBuilder().setSkuDetails(premiumItemInfo).build())
+                       }
+
+
 
 
                    }
